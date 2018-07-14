17552b016a8c4d67a11e0740b107f7292b37c08e
Rinoahu <xiaohu@iastate.edu>
2018-07-14 08:29:17 -0500

    fixed negative scores bugs

--- bin/mcl_sparse.py
+++ bin/mcl_sparse.py
@@ -519,9 +519,9 @@ def csrmm_ez(a, b, mm='msav', cpu=1, prefix=None, tmp_path=None):
     yr, yc, y = b.indptr, b.indices, b.data
     print 'a nnz', a.nnz, 'b nnz', b.nnz
     st = time()
-    if cpu > 1 and x.size > 1e8:
+    if cpu > 1 and x.size > 5e8:
         csrmm = csrmm_sp
-    if cpu > 1 and x.size < 1e8:
+    if cpu > 1 and x.size < 5e8:
         csrmm = csrmm_msav
     elif mm == 'msav':
         csrmm = csrmm_msav
