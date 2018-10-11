b7303a038d56f1dfea3898a90d401e197d81c81b
Rinoahu <xiaohu@iastate.edu>
2018-10-11 16:51:53 -0500

    change the name of default tmpdir

---
+++ upload.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#cd SwiftOrtho
+#cd FastClust
+rm -f ./lib/fsearch-c
+rm -rf ./tmpdir ./bin/__pycache__
+find ./ -name *un~ | xargs rm
+cd example
+rm -f test.fsa.sc*
+rm -rf test.fsa_results/
+rm -rf select.fsa_results/
+cd ..
+
+git config --global user.email xiaohu@iastate.edu
+git config --global user.name Rinoahu
+
+
+git remote rm origin
+
+git add -A .
+git commit -m 'change the name of default tmpdir'
+git remote add origin https://github.com/Rinoahu/SwiftOrtho
+
+git pull origin master
+git push origin master
+
+git checkout master
