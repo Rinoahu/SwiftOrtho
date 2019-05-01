8e6bd4c94c23ca6feba736eefa43f3bae4963ca3
Rinoahu <xiaohu@iastate.edu>
2019-05-01 12:39:18 -0500

    add more tests in example

--- example/run.sh
+++ example/run.sh
@@ -3,8 +3,8 @@
 a=ref.fsa
 
 #python=pypy
-#python=python3
-python=/home/xiaoh/Downloads/compiler/intel/intelpython27/envs/intelpython/bin/python3.5
+python=python3.6
+#python=/home/xiaoh/Downloads/compiler/intel/intelpython27/envs/intelpython/bin/python3.5
 #python=python
 
 rm -rf ref.fsa.sc ref.fsa.sc.orth ref.fsa.sc.orth*mcl ref.fsa.sc.orth*apc ref.fsa.sc_tmp ref.fsa_results
