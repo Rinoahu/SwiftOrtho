0f86f6eaf5cc84e93f9148a711a03e34bd9d4882
Rinoahu <xiaohu@iastate.edu>
2019-05-09 11:27:43 -0500

    add a new script that can merge multiple fasta files into a single one

--- example/run.sh
+++ example/run.sh
@@ -5,7 +5,7 @@ a=ref.fsa
 #python=pypy
 #python=python3.6
 #python=/home/xiaoh/Downloads/compiler/intel/intelpython27/envs/intelpython/bin/python3.5
-#python=python
+python=python
 
 rm -rf ref.fsa.sc ref.fsa.sc.orth ref.fsa.sc.orth*mcl ref.fsa.sc.orth*apc ref.fsa.sc_tmp ref.fsa_results
 
