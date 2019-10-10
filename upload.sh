f88ca13a333b52864675430e2ce9a7732b41a9ab
Rinoahu <xiaohu@iastate.edu>
2019-10-10 14:09:58 -0600

    fixed a bug on cpu without avx support

--- upload.sh
+++ upload.sh
@@ -18,7 +18,7 @@ git config --global user.name Rinoahu
 git remote rm origin
 
 git add -A .
-git commit -m 'using original mcl algorithm in pangenomic pipeline'
+git commit -m 'fixed a bug on cpu without avx support'
 git remote add origin https://github.com/Rinoahu/SwiftOrtho
 
 git pull origin master
