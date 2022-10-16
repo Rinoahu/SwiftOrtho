0795be5f2d0eef87d5fc03829847b04de12191c5
Xiao <xiaohu@iastate.edu>
2022-10-16 16:39:35 -0500

    debug

---
+++ scripts/nr_flt.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python
+# remove redundant sequences before analysis
+
+from Bio import SeqIO
+import sys
+
+
+seqs_dct = {}
+try:
+    qry =sys.argv[1]
+except:
+    qry = sys.stdin
+
+for i in SeqIO.parse(qry, 'fasta'):
+    try:
+        seqs_dct[i.seq].append(i.description)
+    except:
+        seqs_dct[i.seq] = [i.description]
+
+#print(len(seqs_dct))
+#sys.exit()
+
+for i in seqs_dct:
+    print('>' + ' ;;;'.join(seqs_dct[i]))
+    print(i)
