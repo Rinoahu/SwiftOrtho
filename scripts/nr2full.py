e707ac01f5b99dc5dbe54cb7bf2ccf797d14129b
Xiao <xiaohu@iastate.edu>
2022-10-16 17:40:03 -0500

    debug

--- scripts/nr2full.py
+++ scripts/nr2full.py
@@ -14,3 +14,14 @@ except:
 f = open(qry, 'r')
 
 for i in f:
+    j = i[:-1].split('\t')
+    qds, rds = j[-2:]
+    for qd in qs.split(' ;;;')
+        for rd in rs.split(' ;;;'):
+            q = qd.split(' ')[0]
+            r = rd.split(' ')[0]
+            out = [q, r] + j[2:-2] + [qd, rd]
+            print('\t'.join(out))
+
+f.close()
+
