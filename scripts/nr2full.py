c609b27a2a44e1064c5817cb7da2eecba293d9da
Xiao <xiaohu@iastate.edu>
2022-10-17 14:48:12 -0500

    debug

--- scripts/nr2full.py
+++ scripts/nr2full.py
@@ -13,15 +13,23 @@ except:
 
 f = open(qry, 'r')
 
+#for i in f:
+#    j = i[:-1].split('\t')
+#    qds = j[-1]
+#    for qd in qds.split(' ;;;'):
+#            q = qd.split(' ')[0]
+#            out = [q] + j[1:-1] + [qd]
+#            print('\t'.join(out))
+#f.close()
+
 for i in f:
     j = i[:-1].split('\t')
-    qds, rds = j[-2:]
-    for qd in qds.split(' ;;;'):
-        for rd in rds.split(' ;;;'):
+    qds, rds = j[:2]
+    for qd in qds.split(';;;'):
+        for rd in rds.split(';;;'):
             q = qd.split(' ')[0]
             r = rd.split(' ')[0]
             out = [q, r] + j[2:-2] + [qd, rd]
             print('\t'.join(out))
-
 f.close()
 
--- scripts/nr_flt.py
+++ scripts/nr_flt.py
@@ -13,13 +13,15 @@ except:
 
 for i in SeqIO.parse(qry, 'fasta'):
     try:
-        seqs_dct[i.seq].append(i.description)
+        #seqs_dct[i.seq].append(i.description)
+        seqs_dct[i.seq].append(i.id)
     except:
-        seqs_dct[i.seq] = [i.description]
+        #seqs_dct[i.seq] = [i.description]
+        seqs_dct[i.seq] = [i.id]
 
 #print(len(seqs_dct))
 #sys.exit()
 
 for i in seqs_dct:
-    print('>' + ' ;;;'.join(seqs_dct[i]))
+    print('>' + ';;;'.join(seqs_dct[i]))
     print(i)
