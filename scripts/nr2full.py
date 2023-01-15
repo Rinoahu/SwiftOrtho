607b5ce1b316f9087a89f97e2f507db7ebedd592
Xiao <xiaohu@iastate.edu>
2023-01-15 15:07:17 -0600

    debug

--- scripts/nr2full.py
+++ scripts/nr2full.py
@@ -11,7 +11,56 @@ except:
     print('python this.py foo.sc')
     sys.exit()
 
+def blastparse(f):
+    hits = []
+    for i in f:
+        j = i[:-1].split('\t')
+        qds, rds = j[:2]
+        if hits and hits[0][0] != qds:
+            yield hits
+            hits = [j]
+        else:
+            hits.append(j)
+    if hits:
+        yield hits
+
 f = open(qry, 'r')
+for hits in blastparse(f):
+    outs = {}
+    for j in hits:
+        qds, rds = j[:2]
+        for qd in qds.split(';;;'):
+            for rd in rds.split(';;;'):
+                q = qd.split(' ')[0]
+                r = rd.split(' ')[0]
+                out = [q, r] + j[2:-2] + [qd, rd]
+                #print('\t'.join(out))
+                out = '\t'.join(out)
+                try:
+                    outs[q].append(out)
+                except:
+                    outs[q] = [out]
+    for out in outs.values:
+        print(out)
+f.close()
+
+
+
+f = open(qry, 'r')
+for i in f:
+    j = i[:-1].split('\t')
+    qds, rds = j[:2]
+    for qd in qds.split(';;;'):
+        for rd in rds.split(';;;'):
+            q = qd.split(' ')[0]
+            r = rd.split(' ')[0]
+            out = [q, r] + j[2:-2] + [qd, rd]
+            print('\t'.join(out))
+f.close()
+
+
+
+
 
 #for i in f:
 #    j = i[:-1].split('\t')
